79ee3ab14ee812518df4fb882332aa2bfd5d1604
cowbear6598 <cowbear6598@gmail.com>
2023-12-27 11:29:18 +0800

    feat: add plugin and name changer

---
+++ Assets/SoapTools/Editor/Misc/NameChanger.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SoapTools.Editor
+{
+    public class NameChanger : EditorWindow
+    {
+        private string prefixesName;
+        private string suffixesName;
+        private int    startOrder;
+
+        [MenuItem("Soap/物件改名")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<NameChanger>();
+            window.titleContent = new GUIContent("Setting");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField($"Selection Count: {Selection.count}");
+
+            prefixesName = EditorGUILayout.TextField("Prefixes Name: ", prefixesName);
+            suffixesName = EditorGUILayout.TextField("Suffixes Name: ", suffixesName);
+            startOrder   = EditorGUILayout.IntField("Start Order: ", startOrder);
+
+            if (GUILayout.Button("Change"))
+            {
+                Change();
+            }
+        }
+
+        private void Change()
+        {
+            for (int i = 0; i < Selection.transforms.Length; i++)
+            {
+                var selectedObj = Selection.transforms[i];
+
+                selectedObj.name = prefixesName + (startOrder + i) + suffixesName;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# SoapTools
+# Document
+
+- 自己在開發時基本上都會用到的工具或功能，基本上得依賴於 DI 架構，例如：Zenject、VContainer。
+
+# Install
+
+- Open Edit => Porject Settings => Package Manager
+- Add a new Scoped Registry
+
+```
+Name: package.openupm.com
+URL: https://package.openupm.com
+Scope(s): 
+         com.cysharp.unitask
+         com.neuecc.unirx
+```
+
+- Open Window => Package Manager
+- Add package from git URL
+
+```
+https://github.com/cowbear6598/SoapTools.git?path=Assets/SoapTools
+```
+
+# 功能介紹
+
+### 群體改名
+
+- Open Soap => 物件改名
+- 選擇同樣階層的物件們，給他前墜與後墜以及起始編號，就可以一次改完所有物件的名稱。
